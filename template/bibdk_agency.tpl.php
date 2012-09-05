2d5ad37cf9318e25bae14f665d2b737d51fd84f9
pjohans <pjohans@gmail.com>
2012-09-05 08:26:02 +0200

    handle a single agency in seperate template

---
+++ template/bibdk_agency.tpl.php
@@ -0,0 +1,52 @@
+    <div class="agency-container">
+
+      <div class="left-infobox">
+       <?php $branch = $variables['agency']; ?>
+        <p><?php print $branch->branchName; ?></p>
+
+        <?php if (isset($branch->postalAddress) && $branch->postalAddress) : ?>
+          <p><?php print $branch->postalAddress; ?><br/>
+            <?php print $branch->postalCode; ?> <?php print $branch->city; ?><br/>
+            <?php $findVej = $branch->postalAddress.','.$branch->postalCode;?>
+            <a href="http://findvej.dk/<?php print urlencode($findVej); ?>" target="_blank" />Se på kort (findvej.dk)</a></p>
+        <?php endif; ?>
+
+        <?php if (isset($branch->branchPhone) && $branch->branchPhone) : ?>
+          <span>Telefon: <?php print $branch->branchPhone; ?></span>
+        <?php endif; ?>
+
+        <?php if (isset($branch->branchEmail) && $branch->branchPhone) : ?>
+          <p>E-mail: <a href="mailto:<?php print $branch->branchEmail ?>"/><?php print $branch->branchEmail ?></a></p>
+        <?php endif; ?>
+      </div>
+
+      <div class="right-infobox">
+        <?php if (isset($branch->branchWebsiteUrl) && $branch->branchWebsiteUrl) : ?>
+          <p><a href="<?php print $branch->branchWebsiteUrl ?>" target="_blank" />Bibliotekets hjemmeside></a><br/>
+          <?php endif; ?>
+          <?php if (isset($branch->serviceDeclarationUrl) && $branch->serviceDeclarationUrl) : ?>
+            <a href="<?php print $branch->serviceDeclarationUrl ?>" target="_blank" />Servicedeklaration></a></p>
+        <?php endif; ?>
+        <?php if (isset($branch->openingHours) && $branch->openingHours) : ?>
+          <p>Åbningstider:<br/>
+            <?php foreach ($branch->openingHours as $hours) : ?>
+              <?php print $hours->{'$'}; ?>
+              <br/>
+            <?php endforeach; ?>
+          </p>
+        <?php endif; ?>
+      </div>
+
+<?php
+// give each branch a unique classname for messages to show correct
+echo '<div class="messages_' .$branch->branchId. '"></div>';
+?>
+
+   <div>
+   <!-- additional html; buttons etc. -->
+ <?php foreach($branch->addit as $addit) : ?>
+     <div><?php print $addit; ?></div>
+ <?php endforeach; ?>
+
+      <hr/>
+    </div>
--- template/bibdk_vejviser.tpl.php
+++ template/bibdk_vejviser.tpl.php
@@ -10,51 +10,11 @@
 <h4>Biblioteksvejviser - resultat - vælg bibliotek</h4>
 <p>i alt: <?php print $result['count']; ?></p>
 <hr/>
-<?php foreach ($result['libraries'] as $agency) : ?>
-  <?php foreach ($agency->pickUpAgencies as $branch) : ?>
+ <?php foreach ($result['libraries'] as $agency) : ?>
+ <?php foreach ($agency->pickUpAgencies as $branch) : ?>
 
-    <div class="agency-container">
+ <?php print theme('bibdk_agency',array('agency'=>$branch));?>
 
-      <div class="left-infobox">
-        <p><?php print $branch->branchName; ?><br/>
-          (<?php print $agency->agencyName; ?>)</p>
-
-        <?php if (isset($branch->postalAddress) && $branch->postalAddress) : ?>
-          <p><?php print $branch->postalAddress; ?><br/>
-            <?php print $branch->postalCode; ?> <?php print $branch->city; ?><br/>
-            <?php $findVej = $branch->postalAddress.','.$branch->postalCode;?>
-            <a href="http://findvej.dk/<?php print urlencode($findVej); ?>" target="_blank" />Se på kort (findvej.dk)</a></p>
-        <?php endif; ?>
-
-        <?php if (isset($branch->branchPhone) && $branch->branchPhone) : ?>
-          <span>Telefon: <?php print $branch->branchPhone; ?></span>
-        <?php endif; ?>
-
-        <?php if (isset($branch->branchEmail) && $branch->branchPhone) : ?>
-          <p>E-mail: <a href="mailto:<?php print $branch->branchEmail ?>"/><?php print $branch->branchEmail ?></a></p>
-        <?php endif; ?>
-      </div>
-
-      <div class="right-infobox">
-        <?php if (isset($branch->branchWebsiteUrl) && $branch->branchWebsiteUrl) : ?>
-          <p><a href="<?php print $branch->branchWebsiteUrl ?>" target="_blank" />Bibliotekets hjemmeside></a><br/>
-          <?php endif; ?>
-          <?php if (isset($branch->serviceDeclarationUrl) && $branch->serviceDeclarationUrl) : ?>
-            <a href="<?php print $branch->serviceDeclarationUrl ?>" target="_blank" />Servicedeklaration></a></p>
-        <?php endif; ?>
-        <?php if (isset($branch->openingHours) && $branch->openingHours) : ?>
-          <p>Åbningstider:<br/>
-            <?php foreach ($branch->openingHours as $hours) : ?>
-              <?php print $hours->{'$'}; ?>
-              <br/>
-            <?php endforeach; ?>
-          </p>
-        <?php endif; ?>
-      </div>
- <div><?php print drupal_render($branch->button);?></div>
-      <hr/>
-    </div>
-
-  <?php endforeach; ?>
-<?php endforeach; ?>
-<?php endif;?>
+ <?php endforeach; ?>
+ <?php endforeach; ?>
+ <?php endif;?>
