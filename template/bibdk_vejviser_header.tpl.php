2a73d7bb237592b26d9d4b58f9759b7413430286
JrgenGNielsen <jgn@dbc.dk>
2013-03-20 13:55:22 +0100

    add truncation and counter in library directory search result header

---
+++ template/bibdk_vejviser_header.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ */
+?>
+<div id="bibdk-vejviser-header">
+  <div id="bibdk-vejviser-count"><?php print t('@count records', array('@count' => $count), array('context' => 'bibdk_vejviser')); ?></div>
+  <?php print $search_form; ?>
+</div>
