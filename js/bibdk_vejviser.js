390c44d81f2574f0559ee43fa781b5e96b7f033b
Lars Hansen <lkh@dbc.dk>
2019-01-31 09:47:28 +0100

    Fixed the vejviser so you can copy the address etc.

--- js/bibdk_vejviser.js
+++ js/bibdk_vejviser.js
@@ -2,10 +2,12 @@
 
   Drupal.behaviors.bibdkVejviserOnChange = {
     attach: function(context, settings) {
-      $('input[name="openagency_query"]').change(function() {
+
+      var openagency_query = $("input[name='openagency_query']");
+
+      openagency_query.change(function() {
         var value = $(this).val();
-        value = value.replace('*', '?');
-        $(this).val(value);
+        $(this).val(value.replace("*", "?"));
       });
     }
   };
