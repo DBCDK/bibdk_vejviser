cf325cd45c42b97dddbe8e5adde9fd5b36bc7000
pjohans <pjohans@gmail.com>
2019-02-06 14:54:48 +0100

    that took some time

--- js/bibdk_vejviser.js
+++ js/bibdk_vejviser.js
@@ -1,4 +1,14 @@
 (function($) {
+  var bibdkVejviser = {}
+
+  bibdkVejviser.sleepAndGo = function(methodToExecute, time){
+    setTimeout(methodToExecute, time)
+  };
+
+  bibdkVejviser.clickSubmit = function(){
+    var but = $('#edit-openagency-submit');
+    but.click();
+  };
 
   Drupal.behaviors.bibdkVejviserOnChange = {
     attach: function(context, settings) {
@@ -8,6 +18,13 @@
       openagency_query.change(function() {
         var value = $(this).val();
         $(this).val(value.replace("*", "?"));
+      });
+
+      openagency_query.keyup(function(event){
+        if(event.keyCode === 13) {
+          // wait 0,01 sec to make the autocomplete go away
+          bibdkVejviser.sleepAndGo(bibdkVejviser.clickSubmit, 100)
+        }
       });
     }
   };
